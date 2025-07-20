125b4cba85f0c1081c5be428d7f7b3f1cf37f595
y-soliloquy <engineer.soliloquy@gmail.com>
2025-07-20 13:16:15 +0900

    /helloを用意する

--- app/main.go
+++ app/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/y-soliloquy/kintore-pocket-backend/app/router"
+)
+
+func main() {
+	r := router.NewRouter()
+
+	log.Println("Listening on :8080")
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
