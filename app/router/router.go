91e143c53d46747db1aeb72296ab806649bd6eca
y-soliloquy <engineer.soliloquy@gmail.com>
2025-07-20 13:03:08 +0900

    hello worldを返すパスを持ったルーターを作成

--- app/router/router.go
+++ app/router/router.go
@@ -1,1 +1,17 @@
 package router
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func NewRouter() http.Handler {
+	r := chi.NewRouter()
+
+	r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+
+	return r
+}
