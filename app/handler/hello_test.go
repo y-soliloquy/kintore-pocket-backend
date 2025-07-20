51aa0f4d3c6b4d3dffb9bca3dd96e51dc2556ee5
y-soliloquy <engineer.soliloquy@gmail.com>
2025-07-20 14:52:54 +0900

    hello.goのテスト作成

--- app/handler/hello_test.go
+++ app/handler/hello_test.go
@@ -1,1 +1,30 @@
 package handler_test
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/y-soliloquy/kintore-pocket-backend/app/handler"
+)
+
+func TestHelloHandler_Handle(t *testing.T) {
+	h := handler.NewHelloHandler()
+
+	req := httptest.NewRequest(http.MethodGet, "/hello", nil)
+	w := httptest.NewRecorder()
+
+	h.Handle(w, req)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	body, _ := io.ReadAll(res.Body)
+	got := strings.TrimSpace(string(body))
+	want := "hello world"
+	if got != want {
+		t.Errorf("expected body %q, got %q", want, got)
+	}
+}
