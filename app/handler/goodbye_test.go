866b1ba54adc7170a07cf92a299c929223f96696
y-soliloquy <engineer.soliloquy@gmail.com>
2025-07-20 14:54:38 +0900

    goodbye.goのテスト作成

--- app/handler/goodbye_test.go
+++ app/handler/goodbye_test.go
@@ -1,1 +1,30 @@
 package handler_test
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/y-soliloquy/kintore-pocket-backend/app/handler"
+)
+
+func TestGoodbyeHandler_Handle(t *testing.T) {
+	h := handler.NewGoodbyeHandler()
+
+	req := httptest.NewRequest(http.MethodGet, "/goodbye", nil)
+	w := httptest.NewRecorder()
+
+	h.Handle(w, req)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	body, _ := io.ReadAll(res.Body)
+	got := strings.TrimSpace(string(body))
+	want := "Goodbye"
+	if got != want {
+		t.Errorf("expected body %q, got %q", want, got)
+	}
+}
