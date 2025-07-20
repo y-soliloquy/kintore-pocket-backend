4b2caacb48c5a86cbe45712819acd2e2b27a218a
y-soliloquy <engineer.soliloquy@gmail.com>
2025-07-20 13:51:00 +0900

    ハンドラーをルーターに仕込む

--- app/router/router.go
+++ app/router/router.go
@@ -4,14 +4,26 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/y-soliloquy/kintore-pocket-backend/app/handler"
 )
+
+type Route struct {
+	Method  string
+	Path    string
+	Handler http.HandlerFunc
+}
 
 func NewRouter() http.Handler {
 	r := chi.NewRouter()
 
-	r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
-	})
+	routes := []Route{
+		{Method: http.MethodGet, Path: "/hello", Handler: handler.NewHelloHandler().Handle},
+		{Method: http.MethodGet, Path: "/goodbye", Handler: handler.NewGoodbyeHandler().Handle},
+	}
+
+	for _, route := range routes {
+		r.Method(route.Method, route.Path, route.Handler)
+	}
 
 	return r
 }
