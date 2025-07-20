60057801bd64fcbe8fb0f4fcd76717e9f76cb301
y-soliloquy <engineer.soliloquy@gmail.com>
2025-07-20 13:39:36 +0900

    /helloのハンドラーに拡張性を持たせておく

--- app/handler/hello.go
+++ app/handler/hello.go
@@ -1,1 +1,19 @@
 package handler
+
+import (
+	"net/http"
+)
+
+type HelloHandler struct {
+	// dbやconfigを入れて拡張できるようにして多く
+	// db *sql.DB
+}
+
+func NewHelloHandler() *HelloHandler {
+	return &HelloHandler{}
+}
+
+func (h *HelloHandler) Handle(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("hello world"))
+}
