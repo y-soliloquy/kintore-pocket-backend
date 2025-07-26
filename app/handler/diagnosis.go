a83e2f66117fb7b45ff07c4308e6f03adfb73d6b
y-soliloquy <engineer.soliloquy@gmail.com>
2025-07-26 18:13:50 +0900

    ハンドラーを仮実装

--- app/handler/diagnosis.go
+++ app/handler/diagnosis.go
@@ -1,1 +1,16 @@
 package handler
+
+import "net/http"
+
+type DiagnosisHandler struct {
+	// dbやconfigを入れて拡張できるようにして多く
+	// db *sql.DB
+}
+
+func NewDiagnosisHandler() *DiagnosisHandler {
+	return &DiagnosisHandler{}
+}
+
+func (h *DiagnosisHandler) Handle(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
