4a655651ebe256e4162ed3caae9b49fdd1b5acf3
y-soliloquy <engineer.soliloquy@gmail.com>
2025-07-26 17:22:00 +0900

    質問のjsonを読み込む処理

--- app/handler/questions.go
+++ app/handler/questions.go
@@ -1,9 +1,22 @@
 package handler
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 )
+
+type Option struct {
+	Label string `json:"label"`
+	Type  string `json:"type"` // "A"、"B"、もしくは"C"
+}
+
+type Question struct {
+	ID      string   `json:"id"`
+	Title   string   `json:"title"`
+	Options []Option `json:"options"`
+}
 
 type QuestionsHandler struct {
 	// dbやconfigを入れて拡張できるようにして多く
@@ -18,6 +31,22 @@ func (h *QuestionsHandler) Handle(w http.ResponseWriter, r *http.Response) {
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte("questions"))
 	if err != nil {
-		log.Printf("HelloHandler: failed to write response: %v", err)
+		log.Printf("QuestionsHandler: failed to write response: %v", err)
 	}
+}
+
+func LoadQuestions(path string) ([]Question, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		log.Printf("QuestionsHandler: failed to load json: %v", err)
+		return nil, err
+	}
+
+	var questions []Question
+	if err := json.Unmarshal(data, &questions); err != nil {
+		log.Printf("QuestionsHandler: failed to unmarshal json: %v", err)
+		return nil, err
+	}
+
+	return questions, nil
 }
