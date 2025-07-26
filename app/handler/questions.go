e54fb9b2422a572e67a0858b95b7fa76705b5e9f
y-soliloquy <engineer.soliloquy@gmail.com>
2025-07-26 17:02:23 +0900

    ハンドラーを仮実装

--- app/handler/questions.go
+++ app/handler/questions.go
@@ -1,1 +1,23 @@
 package handler
+
+import (
+	"log"
+	"net/http"
+)
+
+type QuestionsHandler struct {
+	// dbやconfigを入れて拡張できるようにして多く
+	// db *sql.DB
+}
+
+func NewQuestionsHandler() *QuestionsHandler {
+	return &QuestionsHandler{}
+}
+
+func (h *QuestionsHandler) Handle(w http.ResponseWriter, r *http.Response) {
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("questions"))
+	if err != nil {
+		log.Printf("HelloHandler: failed to write response: %v", err)
+	}
+}
