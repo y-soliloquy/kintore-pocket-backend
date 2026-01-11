7751e3d87d0cd3a0d975ac59a81469f4bd6cabf5
y-soliloquy <engineer.soliloquy@gmail.com>
2026-01-11 16:46:41 +0900

    文字列を返却するように修正中

--- app/handler/reference.go
+++ app/handler/reference.go
@@ -1,6 +1,9 @@
 package handler
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 // 使うかわからないが拡張できるようにしておく
 type ReferenceHandler struct{}
@@ -11,4 +14,8 @@ func NewReferenceHandler() *ReferenceHandler {
 
 func (h *ReferenceHandler) Handle(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("reference!!"))
+	if err != nil {
+		log.Printf("ReferenceHandler: failed to write response: %v", err)
+	}
 }
