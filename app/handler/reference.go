976c78c43ed0a4b7cdc63b83b431eb74ad3964b8
y-soliloquy <engineer.soliloquy@gmail.com>
2026-01-11 17:06:05 +0900

    jsonファイルをハンドラーで読み込み

--- app/handler/reference.go
+++ app/handler/reference.go
@@ -1,21 +1,46 @@
 package handler
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
-// 使うかわからないが拡張できるようにしておく
-type ReferenceHandler struct{}
+type MovieURL struct {
+	URL string `json:"url"`
+}
 
-func NewReferenceHandler() *ReferenceHandler {
-	return &ReferenceHandler{}
+// 使うかわからないが拡張できるようにしておく
+type ReferenceHandler struct {
+	baseDir string
+}
+
+func NewReferenceHandler(path string) *ReferenceHandler {
+	return &ReferenceHandler{baseDir: path}
 }
 
 func (h *ReferenceHandler) Handle(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	_, err := w.Write([]byte("reference!!"))
+	path := filepath.Join(h.baseDir, "reference.json")
+
+	b, err := os.ReadFile(path)
 	if err != nil {
+		log.Printf("ReferenceHandler: failed to read file: %v", err)
+		http.Error(w, "failed to read reference json", http.StatusInternalServerError)
+		return
+	}
+
+	var urls []MovieURL
+	if err := json.Unmarshal(b, &urls); err != nil {
+		log.Printf("ReferenceHandler: failed to parse json: %v", err)
+		http.Error(w, "invalid reference json", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(urls); err != nil {
 		log.Printf("ReferenceHandler: failed to write response: %v", err)
 	}
 }
